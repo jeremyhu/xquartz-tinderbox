a78541e0800e3a963e7f151c7c4e9960359d9432
Jeremy Huddleston Sequoia <jeremyhu@freedesktop.org>
2010-02-02 01:05:13 +0000

    Store the tinderbox build-foo in svn in preparation for the eventual demise of my system storage
    git-svn-id: https://svn.macosforge.org/repository/xquartz/tinderbox@413 9052f27e-8171-453d-b9bb-b0e8541e09c8

---
+++ tinderbox_test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+. /etc/profile
+. /etc/bashrc
+. ${HOME}/.profile
+. ${HOME}/.bashrc
+
+. ${HOME}/src/strip.sh
+unset CFLAGS OBJCFLAGS CPPFLAGS LDFLAGS C_INCLUDE_PATH OBJC_INCLUDE_PATH CPLUS_INCLUDE_PATH PKG_CONFIG_PATH
+
+#URL="http://jeremyhu-vincent:xQUGcg@tinderbox.x.org/builds/rpc"
+URL="http://jeremyhu-yuffie:xQUGcg@tinderbox.x.org/builds/rpc"
+
+rm /Users/jeremy/src/freedesktop/jhbuild/configure-cache
+
+export PKG_CONFIG_PATH=/usr/lib/pkgconfig:$PKG_CONFIG_PATH
+
+
+#jhbuild clean
+
+#jhbuild build --autogen --clean # --start-at=xkeyboard-config
+jhbuild autobuild --autogen --clean --verbose --report-url="${URL}"
+
+/bin/ls -1 /var/tmp | /usr/bin/head -n 9000 | /usr/bin/grep dSYM | /usr/bin/sed 's:^:/var/tmp/:' | /usr/bin/xargs /bin/rm -rf
+/bin/ls -1 /var/tmp | /usr/bin/head -n 9000 | /usr/bin/grep dSYM | /usr/bin/sed 's:^:/var/tmp/:' | /usr/bin/xargs /bin/rm -rf
