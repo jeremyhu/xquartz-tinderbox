df01bac8038d30f84f046245779038d870987877
Jeremy Huddleston Sequoia <jeremyhu@freedesktop.org>
2010-11-14 10:57:17 +0000

    tinderbox updates
    git-svn-id: https://svn.macosforge.org/repository/xquartz/tinderbox@497 9052f27e-8171-453d-b9bb-b0e8541e09c8

--- tinderbox_test.sh
+++ tinderbox_test.sh
@@ -8,18 +8,20 @@
 . ${HOME}/src/strip.sh
 unset CFLAGS OBJCFLAGS CPPFLAGS LDFLAGS C_INCLUDE_PATH OBJC_INCLUDE_PATH CPLUS_INCLUDE_PATH PKG_CONFIG_PATH
 
+PATH="$PATH:/opt/local/bin"
+
 #URL="http://jeremyhu-vincent:xQUGcg@tinderbox.x.org/builds/rpc"
 URL="http://jeremyhu-yuffie:xQUGcg@tinderbox.x.org/builds/rpc"
 
 rm /Users/jeremy/src/freedesktop/jhbuild/configure-cache
 
-export PKG_CONFIG_PATH=/usr/lib/pkgconfig:$PKG_CONFIG_PATH
-
-
 #jhbuild clean
 
-#jhbuild build --autogen --clean # --start-at=xkeyboard-config
+#jhbuild build --start-at=libX11
+#jhbuild autobuild --autogen --verbose --report-url="${URL}"
 jhbuild autobuild --autogen --clean --verbose --report-url="${URL}"
 
 /bin/ls -1 /var/tmp | /usr/bin/head -n 9000 | /usr/bin/grep dSYM | /usr/bin/sed 's:^:/var/tmp/:' | /usr/bin/xargs /bin/rm -rf
 /bin/ls -1 /var/tmp | /usr/bin/head -n 9000 | /usr/bin/grep dSYM | /usr/bin/sed 's:^:/var/tmp/:' | /usr/bin/xargs /bin/rm -rf
+
+rm -rf /Users/jeremy/src/freedesktop/jhbuild/build/Applications
