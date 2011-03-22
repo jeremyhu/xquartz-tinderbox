651bb2dffcbef5cb6c13c480de1a719cc21abf27
Jeremy Huddleston Sequoia <jeremyhu@freedesktop.org>
2011-03-22 22:55:07 +0000

    tinderbox updates: using clang, building branches, etc
    git-svn-id: https://svn.macosforge.org/repository/xquartz/tinderbox@512 9052f27e-8171-453d-b9bb-b0e8541e09c8

--- tinderbox_test.sh
+++ tinderbox_test.sh
@@ -8,7 +8,7 @@
 . ${HOME}/src/strip.sh
 unset CFLAGS OBJCFLAGS CPPFLAGS LDFLAGS C_INCLUDE_PATH OBJC_INCLUDE_PATH CPLUS_INCLUDE_PATH PKG_CONFIG_PATH
 
-PATH="$PATH:/opt/local/bin"
+PATH="/opt/llvm/bin:/opt/local/bin:$PATH"
 
 #URL="http://jeremyhu-vincent:xQUGcg@tinderbox.x.org/builds/rpc"
 URL="http://jeremyhu-yuffie:xQUGcg@tinderbox.x.org/builds/rpc"
@@ -17,11 +17,17 @@ rm /Users/jeremy/src/freedesktop/jhbuild/configure-cache
 
 #jhbuild clean
 
-#jhbuild build --start-at=libX11
-#jhbuild autobuild --autogen --verbose --report-url="${URL}"
-jhbuild autobuild --autogen --clean --verbose --report-url="${URL}"
+#jhbuild build --autogen --clean --start-at=xserver
+jhbuild autobuild --autogen --verbose --report-url="${URL}"
+#jhbuild autobuild --autogen --clean --verbose --report-url="${URL}"
 
-/bin/ls -1 /var/tmp | /usr/bin/head -n 9000 | /usr/bin/grep dSYM | /usr/bin/sed 's:^:/var/tmp/:' | /usr/bin/xargs /bin/rm -rf
-/bin/ls -1 /var/tmp | /usr/bin/head -n 9000 | /usr/bin/grep dSYM | /usr/bin/sed 's:^:/var/tmp/:' | /usr/bin/xargs /bin/rm -rf
+/bin/ls -1 /var/tmp | /usr/bin/head -n 2000 | /usr/bin/grep dSYM | /usr/bin/sed 's:^:/var/tmp/:' | /usr/bin/xargs /bin/rm -rf
+/bin/ls -1 /var/tmp | /usr/bin/head -n 2000 | /usr/bin/grep dSYM | /usr/bin/sed 's:^:/var/tmp/:' | /usr/bin/xargs /bin/rm -rf
+/bin/ls -1 /var/tmp | /usr/bin/head -n 2000 | /usr/bin/grep dSYM | /usr/bin/sed 's:^:/var/tmp/:' | /usr/bin/xargs /bin/rm -rf
+/bin/ls -1 /var/tmp | /usr/bin/head -n 2000 | /usr/bin/grep dSYM | /usr/bin/sed 's:^:/var/tmp/:' | /usr/bin/xargs /bin/rm -rf
+/bin/ls -1 /var/tmp | /usr/bin/head -n 2000 | /usr/bin/grep dSYM | /usr/bin/sed 's:^:/var/tmp/:' | /usr/bin/xargs /bin/rm -rf
+/bin/ls -1 /var/tmp | /usr/bin/head -n 2000 | /usr/bin/grep dSYM | /usr/bin/sed 's:^:/var/tmp/:' | /usr/bin/xargs /bin/rm -rf
+/bin/ls -1 /var/tmp | /usr/bin/head -n 2000 | /usr/bin/grep dSYM | /usr/bin/sed 's:^:/var/tmp/:' | /usr/bin/xargs /bin/rm -rf
+/bin/ls -1 /var/tmp | /usr/bin/head -n 2000 | /usr/bin/grep dSYM | /usr/bin/sed 's:^:/var/tmp/:' | /usr/bin/xargs /bin/rm -rf
 
 rm -rf /Users/jeremy/src/freedesktop/jhbuild/build/Applications
