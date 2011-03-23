4753be546cb04cfe0a6f43cfc8a2b3ec6a4247e4
Jeremy Huddleston Sequoia <jeremyhu@freedesktop.org>
2011-03-23 16:49:15 +0000

    Skip configure caching, add report URLs for new tinderboxes
    git-svn-id: https://svn.macosforge.org/repository/xquartz/tinderbox@514 9052f27e-8171-453d-b9bb-b0e8541e09c8

--- tinderbox_test.sh
+++ tinderbox_test.sh
@@ -13,13 +13,14 @@ PATH="/opt/llvm/bin:/opt/local/bin:$PATH"
 #URL="http://jeremyhu-vincent:xQUGcg@tinderbox.x.org/builds/rpc"
 URL="http://jeremyhu-yuffie:xQUGcg@tinderbox.x.org/builds/rpc"
 
-rm /Users/jeremy/src/freedesktop/jhbuild/configure-cache
+#URL="http://jeremyhu-tifa-linux32:xFDSPr@tinderbox.x.org/builds/rpc"
+#URL="http://jeremyhu-tifa-linux64:JsFKEr4f6@tinderbox.x.org/builds/rpc"
 
 #jhbuild clean
 
 #jhbuild build --autogen --clean --start-at=xserver
-jhbuild autobuild --autogen --verbose --report-url="${URL}"
-#jhbuild autobuild --autogen --clean --verbose --report-url="${URL}"
+#jhbuild autobuild --autogen --verbose --report-url="${URL}"
+jhbuild autobuild --autogen --clean --verbose --report-url="${URL}"
 
 /bin/ls -1 /var/tmp | /usr/bin/head -n 2000 | /usr/bin/grep dSYM | /usr/bin/sed 's:^:/var/tmp/:' | /usr/bin/xargs /bin/rm -rf
 /bin/ls -1 /var/tmp | /usr/bin/head -n 2000 | /usr/bin/grep dSYM | /usr/bin/sed 's:^:/var/tmp/:' | /usr/bin/xargs /bin/rm -rf
